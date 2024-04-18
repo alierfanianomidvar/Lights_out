1cbc2ba9d7ac7c06234e1bccbb10e99505249560
alierfanianomidvar <ali.erfanianomidvar@gmail.com>
2024-04-18 22:12:07 +0200

    Fix: remove the old code.

--- src/Main.java
+++ src/Main.java
@@ -6,111 +6,12 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Main {
-//    public static void main(String[] args) {
-//
-//
-//        int depth = 2;
-//
-//        // Define the initial board state
-//
-////        int[][] initialBoard = {
-////                {2, 1, 2, 1},
-////                {2, 2, 1, 2},
-////                {1, 0, 0, 1},
-////                {2, 0, 1, 1},
-////                {1, 2, 1, 1},
-////                {2, 1, 1, 1}
-////        };
-//        int[][] initialBoard = {
-//                {0, 1, 0, 0},
-//                {0, 1, 1, 0},
-//                {1, 0, 1, 0},
-//                {1, 1, 1, 0},
-//        };
-//
-//        // Define the pieces and their effects
-//        int[][][] pieces = {
-//                {{1, 0}, {1, 1}, {1, 1}},
-//                {{1, 0, 0, 0}, {1, 1, 1, 1}},
-//                {{1, 1, 1}},
-//                {{1}, {1}},
-//                {{1, 1}},
-//                {{1, 1}, {1, 1}, {0, 1}, {0, 1}},
-//                {{0, 0, 1, 1}, {1, 1, 1, 0}},
-//        };
-//
-////        int[][][] pieces = {
-////                {{1, 0, 0, 0}, {1, 0, 1, 0}, {1, 1, 1, 1}, {1, 1, 0, 0},},
-////                {{1, 1, 1}, {1, 0, 1}},
-////                {{1, 1}, {1, 0}},
-////                {{1, 1}, {0, 1}, {0, 1}},
-////                {{1, 1, 0}, {0, 1, 1}, {0, 1, 0}, {0, 1, 0}},
-////                {{1, 1, 0,0}, {0, 1, 1,1}},
-////                {{0, 1, 0}, {0, 1, 0}, {1, 1, 0}, {1, 1, 1}},
-////                {{0, 0, 1, 1}, {0, 0, 1, 1}, {1, 1, 1, 0}},
-////                {{0, 1, 0, 0}, {0, 1, 1, 0}, {1, 0, 1, 0}, {1, 1, 1, 1}},
-////                {{1}, {1}, {1}, {1}},
-////                {{0, 1, 0}, {0, 1, 1}, {0, 1, 0}, {1, 1, 1}},
-////        };
-////        // Define the depth of the puzzle
-////        int depth = 2;
-////
-////        // Define the initial board state
-////        int[][] initialBoard = {
-////                {0, 0, 1},
-////                {0, 1, 1},
-////                {0, 1, 1,}
-////        };
-////
-////        // Define the pieces and their effects
-////        int[][][] pieces = {
-////                {{0, 1}, {1, 1}},
-////                {{1, 1}},
-////                {{0, 1}, {0, 1}, {1, 1}}
-////        };
-//
-//        LightsOut solver = new LightsOut(depth, initialBoard, pieces);
-//
-//        boolean solutionFound = solver.solve(0);
-//        if (solutionFound) {
-//            System.out.println("Solution found!");
-//        } else {
-//            System.out.println("No solution exists.");
-//        }
-//
-//        /**
-//         * row, col -> 0||1 pieces -> 2
-//         * row, col -> 2||0 pieces -> 1
-//         * row, col -> 1||0 pieces -> 0
-//         * */
-//        /**
-//         * row, col -> 2||0 pieces -> 6
-//         * row, col -> 0||1 pieces -> 5
-//         * row, col -> 0||1 pieces -> 4
-//         * row, col -> 2||2 pieces -> 3
-//         * row, col -> 1||0 pieces -> 2
-//         * row, col -> 1||0 pieces -> 1
-//         * row, col -> 0||1 pieces -> 0
-//         * */
-////
-//
-////        LightsOutBfs bfsSolver = new LightsOutBfs(depth, initialBoard, pieces);
-////
-////        boolean bfsSolutionFound = bfsSolver.solve();
-////
-////        if (bfsSolutionFound) {
-////            System.out.println("BFS Solution found!");
-////        } else {
-////            System.out.println("BFS No solution exists.");
-////        }
-//
-//    }
 
     public static void main(String[] args) {
         try {
 
             boolean bfsFlag = false;
-            File inputFile = new File("src\\samples\\03.txt");
+            File inputFile = new File("src\\samples\\04.txt");
             Scanner scanner = new Scanner(inputFile);
 
             // Read the depth of the puzzle
