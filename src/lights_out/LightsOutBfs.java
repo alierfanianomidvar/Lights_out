3d3b7cb5a1ede077fb3482fd1dba4a2946c53d5b
alierfanianomidvar <ali.erfanianomidvar@gmail.com>
2024-04-19 14:21:50 +0200

    Make the bfs code cleaner and use the record class.

--- src/lights_out/LightsOutBfs.java
+++ src/lights_out/LightsOutBfs.java
@@ -1,5 +1,10 @@
 package lights_out;
+
+import lights_out.record.Move;
+import lights_out.record.State;
+
 import java.util.*;
+
 
 public class LightsOutBfs {
     private int[][] board;
@@ -8,7 +13,6 @@ public class LightsOutBfs {
     private final int rows;
     private final int cols;
 
-    // Constructor
     public LightsOutBfs(int depth, int[][] board, int[][][] pieces) {
         this.depth = depth;
         this.board = board;
@@ -24,29 +28,28 @@ public class LightsOutBfs {
 
         State initial = new State(cloneBoard(board), new ArrayList<>(), 0);
         queue.add(initial);
-        // visited the borad and the piece we played.
         visited.add(Arrays.deepToString(board) + "0");
 
         while (!queue.isEmpty()) {
             State current = queue.poll();
 
             // we need to be sure that we are seeing all the pieces and after that we see that did we solve it!
-            if (current.moves.size() == pieces.length && isSolved(current.board)) {
-                printSolution(current.moves);
+            if (current.moves().size() == pieces.length && isSolved(current.board())) {
+                printSolution(current.moves());
                 return true;
             }
 
-            int p = current.nextPieceIndex;
+            int chosenPieceNumber = current.nextPieceIndex();
             for (int row = 0; row < rows; row++) {
                 for (int col = 0; col < cols; col++) {
-                    int[][] newBoard = cloneBoard(current.board);
-                    if (placePiece(newBoard, pieces[p], row, col)) {
-                        String boardKey = Arrays.deepToString(newBoard) + (p + 1) % pieces.length;  // move to the next piece
+                    int[][] newBoard = cloneBoard(current.board());
+                    if (placePiece(newBoard, pieces[chosenPieceNumber], row, col)) {
+                        String boardKey = Arrays.deepToString(newBoard) + (chosenPieceNumber + 1) % pieces.length; // move to the next piece
                         if (!visited.contains(boardKey)) {
                             visited.add(boardKey);
-                            List<Move> newMoves = new ArrayList<>(current.moves);
-                            newMoves.add(new Move(p, row, col));
-                            queue.add(new State(newBoard, newMoves, (p + 1) % pieces.length));
+                            List<Move> newMoves = new ArrayList<>(current.moves());
+                            newMoves.add(new Move(chosenPieceNumber, row, col));
+                            queue.add(new State(newBoard, newMoves, (chosenPieceNumber + 1) % pieces.length));
                         }
                     }
                 }
@@ -55,40 +58,8 @@ public class LightsOutBfs {
         return false;
     }
 
-    private static class State {
-
-        int[][] board;
-        List<Move> moves;
-        int nextPieceIndex;
-
-        State(int[][] board, List<Move> moves, int nextPieceIndex) {
-            this.board = board;
-            this.moves = moves;
-            this.nextPieceIndex = nextPieceIndex;
-
-        }
-    }
-
-    private static class Move {
-        int pieceIndex;
-        int x;
-        int y;
-
-        Move(int pieceIndex, int x, int y) {
-            this.pieceIndex = pieceIndex;
-            this.x = x;
-            this.y = y;
-        }
-
-        @Override
-        public String toString() {
-            return String.format("Piece %d at (%d, %d)", pieceIndex, x, y);
-        }
-    }
-
-    private boolean isSolved(int[][] b) {
-
-        for (int[] row : b) {
+    private boolean isSolved(int[][] inputBoard) {
+        for (int[] row : inputBoard) {
             for (int value : row) {
                 if (value != 0) return false;
             }
@@ -96,21 +67,26 @@ public class LightsOutBfs {
         return true;
     }
 
-    private boolean placePiece(int[][] b, int[][] piece, int startX, int startY) {
-        if (startX + piece.length > b.length || startY + piece[0].length > b[0].length) {
-            return false; // Out of bounds
+    private boolean placePiece(
+            int[][] inputBoard,
+            int[][] piece,
+            int startX,
+            int startY) {
+
+        if (startX + piece.length > inputBoard.length
+                || startY + piece[0].length > inputBoard[0].length) {
+            return false;
         }
 
         boolean placed = false;
         for (int i = 0; i < piece.length; i++) {
             for (int j = 0; j < piece[i].length; j++) {
                 if (piece[i][j] == 1) {
-                    b[startX + i][startY + j] = (b[startX + i][startY + j] + 1) % depth;
+                    inputBoard[startX + i][startY + j] = (inputBoard[startX + i][startY + j] + 1) % depth;
                     placed = true;
                 }
             }
         }
-
         return placed;
     }
 
