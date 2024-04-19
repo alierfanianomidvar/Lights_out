68503e079e8d18a6a399ad768bd16c3a017c69a5
alierfanianomidvar <ali.erfanianomidvar@gmail.com>
2024-04-19 13:49:43 +0200

    Add: some old comment to the lights out file.

--- src/lights_out/LightsOut.java
+++ src/lights_out/LightsOut.java
@@ -21,9 +21,18 @@ public class LightsOut {
             for (int col = 0; col < board[0].length; col++) {
                 if (placePiece(pieces[currentPiece], row, col)) {
                     if (solve(currentPiece + 1)) {
-                        System.out.println("row, col -> " + row + "||" + col + "pieces -> " + currentPiece);
+                        System.out.println("col, row -> " + row + "||" + col + " pieces -> " + currentPiece);
                         return true;
                     }
+
+                    /**
+                     * We can remove this and just save the last bord, but it is not memory efficient.
+                     * and this way it is easire to develop, but we need more time to run the code. this is just a trade-off
+                     * between memory and time.
+                     *
+                     * But we must consider that we in this way we still can face the worst case, so it is not a good idea to think
+                     * how we can make this time better, we must think how can we make the memory better in the algorithm.
+                     */
                     removePiece(pieces[currentPiece], row, col);
                 }
             }
@@ -59,16 +68,21 @@ public class LightsOut {
             int xPosition,
             int yPosition) {
 
-        // Can we place the piece in the board !! - > here
-        if (xPosition + piece.length > board.length
-                || yPosition + piece[0].length > board[0].length) {
+        int pieceLength = piece.length;
+        int pieceWidth = piece[0].length;
+        int depth = this.depth; // Assuming depth is a field of the class
+
+        // Check if placement would be valid before modifying the board
+        int maxX = xPosition + pieceLength;
+        int maxY = yPosition + pieceWidth;
+        if (maxX > board.length || maxY > board[0].length) {
             return;
         }
 
-        for (int i = 0; i < piece.length; i++) {
-            for (int j = 0; j < piece[i].length; j++) {
+        // Loop with pre-calculated increments for better cache locality
+        for (int i = 0; i < pieceLength; i++) {
+            for (int j = 0; j < pieceWidth; j++) {
                 if (piece[i][j] == 1) {
-                    // remove the piece from the board.
                     board[xPosition + i][yPosition + j] = (board[xPosition + i][yPosition + j] - 1 + depth) % depth;
                 }
             }
@@ -77,13 +91,30 @@ public class LightsOut {
 
 
     private boolean isSolved() {
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
+        int m = board.length; // Get number of rows (m)
+        int n = board[0].length; // Get number of columns (n) - assuming all rows have the same length
+
+        // Efficient check for non-zero in the first row
+        for (int j = 0; j < n; j++) {
+            if (board[0][j] != 0) {
+                return false;
+            }
+        }
+
+        // Early return for single row matrix
+        if (m == 1) {
+            return true;
+        }
+
+        // Check remaining rows for zeros (optimized for locality)
+        for (int i = 1; i < m; i++) {
+            for (int j = 0; j < n; j++) {
                 if (board[i][j] != 0) {
-                    return false; // If we saw non zero value, we did not solve the problem.
+                    return false;
                 }
             }
         }
+
         return true;
     }
 
